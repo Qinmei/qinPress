f9efc1a8cf003f7e189c0c93843034f3558380c7
qinvz <sun.houtao@outlook.com>
2018-11-02 23:40:30 +0800

    BUG add

--- functions.php
+++ functions.php
@@ -2,38 +2,6 @@
 require get_template_directory() . '/ashuwp_framework/ashuwp_framework_core.php'; //加载ashuwp_framework框架
 require get_template_directory() . '/ashuwp_framework/config-example.php'; //加载配置数据，config-example.php为配置范例。
 
-
-add_action( 'rest_api_init', function() {
-    
-  remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-  add_filter( 'rest_pre_serve_request', function( $value ) {
-    header("Content-Type:text/html;charset=utf8"); 
-    header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
-    header('Access-Control-Allow-Headers:*');
-    header( 'Access-Control-Allow-Credentials: true' );
-
-    $general_options = get_option('ashuwp_general');
-    $allow=array(
-      stripslashes($general_options["qinmei_allow_site_web"]),
-      stripslashes($general_options["qinmei_allow_site_mobile"]),
-      'https://qinvz.cn',
-    );
-
-
-
-    $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
-    if (in_array($origin, $allow)) {
-      header("Access-Control-Allow-Origin:".$origin);
-    }
-
-    return $value;
-    
-  });
-}, 15 ); 
-
-
-  
-
 require get_template_directory() . '/qinmei/animate.php';
 require get_template_directory() . '/qinmei/longpost.php';
 require get_template_directory() . '/qinmei/comment.php';
